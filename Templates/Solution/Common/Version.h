7ed7bb3a6b9ad15499847f989519f1f6518c1df0
unknown <mdubois@Ti.kms-quebec.com>
2014-10-27 16:39:14 -0400

    Templace - Solution

---
+++ Templates/Solution/Common/Version.h
@@ -0,0 +1,20 @@
+
+// Auteur	:	KMS -	Martin Dubois, ing.
+// Cleint	:	TODO
+// Projet	:	TODO
+// Fichier	:	Common/Version.h
+
+#pragma once
+
+// Constantes
+/////////////////////////////////////////////////////////////////////////////
+
+#define	VERSION_STR	"0.0.0.0"
+
+#define VERSION_MAJOR			(0)
+#define VERSION_MINOR			(0)
+#define VERSION_BUILD			(0)
+#define VERSION_COMPATIBILITY	(0)
+
+#define VERSION_INTERNAL	(true)
+

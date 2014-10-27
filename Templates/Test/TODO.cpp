a2008c8f997bc1f531738bd1500e2c8b91d8ce2d
unknown <mdubois@Ti.kms-quebec.com>
2014-10-27 16:39:53 -0400

    Template - Test

---
+++ Templates/Test/TODO.cpp
@@ -0,0 +1,18 @@
+
+// Auteur	:	KMS -	Martin Dubois, ing.
+// Client	:	TODO
+// Projet	:	TODO
+// Fichier	:	TODO_Test/TODO_Test.cpp
+
+// Includes
+/////////////////////////////////////////////////////////////////////////////
+
+// ===== KmsBase ============================================================
+#include <KmsTest.h>
+
+// Tests
+/////////////////////////////////////////////////////////////////////////////
+
+KMS_TEST_BEGIN(TODO_Base)
+
+KMS_TEST_END
---
+++ Templates/Test/TODO_Test.cpp
@@ -0,0 +1,26 @@
+
+// Auteur	:	KMS -	Martin Dubois, ing.
+// Client	:	TODO
+// Projet	:	TODO
+// Fichier	:	TODO_Test/TODO_Test.cpp
+
+// Includes
+/////////////////////////////////////////////////////////////////////////////
+
+// ===== KmsBase ============================================================
+#include <KmsTest.h>
+
+// Test
+/////////////////////////////////////////////////////////////////////////////
+
+KMS_TEST_GROUP_LIST_BEGIN
+	KMS_TEST_GROUP_LIST_ENTRY("TODO"	)
+KMS_TEST_GROUP_LIST_END
+
+extern int TODO_Base	();
+
+KMS_TEST_LIST_BEGIN
+	KMS_TEST_LIST_ENTRY(TODO_Base	, "TODO - Base"		, 0, 0									)
+KMS_TEST_LIST_END
+
+KMS_TEST_MAIN

912d413d481c5b2cf77bde6cd1a3ff17c2f89d7b
unknown <mdubois@Ti.kms-quebec.com>
2015-11-26 16:31:11 -0500

    2.4.5

--- Common/Version.h
+++ Common/Version.h
@@ -1,20 +1,20 @@
 
-// Author / Auteur		:	KMS -	Martin Dubois, ing.
-// Product / Produit	:	KmsBase
-// File / Fichier		:	Common/Version.h
+// Author / Auteur		KMS	-	Martin Dubois, ing.
+// Product / Produit	KmsBase
+// File / Fichier		Common/Version.h
 
 #pragma once
 
 // Constantes
 /////////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC		2,4,4,0
-#define	VERSION_STR		"2.4.4.0"
-#define	VERSION_STR0	"2.4.4.0\0"
+#define VERSION_RC		2,4,5,0
+#define	VERSION_STR		"2.4.5.0"
+#define	VERSION_STR0	"2.4.5.0\0"
 
 #define VERSION_MAJOR			(2)
 #define VERSION_MINOR			(4)
-#define VERSION_BUILD			(4)
+#define VERSION_BUILD			(5)
 
 #define VERSION_COMPATIBILITY	(0)
 

6c8d47186d38e0956bcdd8b0c4e15074112926ba
unknown <mdubois@Ti.kms-quebec.com>
2015-12-23 09:57:37 -0500

    2.4.10

--- Common/Version.h
+++ Common/Version.h
@@ -8,13 +8,13 @@
 // Constantes
 /////////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC		2,4,9,0
-#define	VERSION_STR		"2.4.9.0"
-#define	VERSION_STR0	"2.4.9.0\0"
+#define VERSION_RC		2,4,10,0
+#define	VERSION_STR		"2.4.10.0"
+#define	VERSION_STR0	"2.4.10.0\0"
 
 #define VERSION_MAJOR			(2)
 #define VERSION_MINOR			(4)
-#define VERSION_BUILD			(9)
+#define VERSION_BUILD			(10)
 
 #define VERSION_COMPATIBILITY	(0)
 

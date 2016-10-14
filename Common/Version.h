a0453cbd36e6d6602dc2223c3f06ce7c3588b150
unknown <mdubois@Be.kms-quebec.com>
2016-10-13 20:15:36 -0400

    2.4.17

--- Common/Version.h
+++ Common/Version.h
@@ -1,21 +1,21 @@
 
-// Author / Auteur		KMS	-	Martin Dubois, ing.
-// Product / Produit	KmsBase
-// File / Fichier		Common/Version.h
+// Author / Auteur     KMS - Martin Dubois, ing.
+// Product / Produit   KmsBase
+// File / Fichier      Common/Version.h
 
-#pragma once
+#pragma  once
 
 // Constantes
 /////////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC		2,4,16,0
-#define	VERSION_STR		"2.4.16.0"
-#define	VERSION_STR0	"2.4.16.0\0"
+#define  VERSION_RC     2,4,17,0
+#define	 VERSION_STR    "2.4.17.0"
+#define	 VERSION_STR0   "2.4.17.0\0"
 
-#define VERSION_MAJOR			(2)
-#define VERSION_MINOR			(4)
-#define VERSION_BUILD			(16)
+#define  VERSION_MAJOR   (2)
+#define  VERSION_MINOR   (4)
+#define  VERSION_BUILD   (17)
 
-#define VERSION_COMPATIBILITY	(0)
+#define  VERSION_COMPATIBILITY  (0)
 
-#define VERSION_INTERNAL	(false)
+#define  VERSION_INTERNAL  (true)

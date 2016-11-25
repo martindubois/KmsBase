3eebfb7086c6db1c12f3b980b99e929b874e3ef1
unknown <mdubois@Be.kms-quebec.com>
2016-11-24 19:55:47 -0500

    Remove chocolatey package

--- kms.base.internal/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-# Author / Auteur	KMS	-	Martin Dubois, ing.
-# Product / Produit	KmsBase
-# File / Fichier	kms.base/tools/chocolateyuninstall.ps1
-
-
-Remove-Item C:\Software\KmsBase_2.4 -recurse

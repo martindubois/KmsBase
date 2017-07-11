95dad99d89d8dd130d1b81cf1260b9f760bb040c
Martin Dubois <mdubois@kms-quebec.com>
2017-07-10 20:07:56 -0400

    2.5.0

---
+++ Scripts/Import.cmd
@@ -0,0 +1,53 @@
+@echo off
+
+rem  Author / Auteur     KMS - Martin Dubois, ing.
+rem  Product / Produit   KmsBase
+rem  File / Fichier      Scripts/Import.cmd
+
+echo  Executing  Import.cmd %1  ...
+
+rem  ===== Arguments ========================================================
+
+set DST=%1
+
+rem  ===== Constants ========================================================
+
+set KMS_COPY_EXE=Binaries/Release_32/KmsCopy.exe
+
+set IMPORT_CMD_TXT=Import.cmd.txt
+
+rem  ===== Verification =====================================================
+
+if not exist %DST% (
+    echo  USER ERROR  %DST% does not exist
+    pause
+    exit /B 1
+)
+
+if not exist %KMS_COPY_EXE% (
+    echo  FATAL ERROR  %KMS_COPY_EXE% does not exist
+    echo  The package is not complete
+    pause
+    exit /B 2
+)
+
+if not exist %IMPORT_CMD_TXT% (
+    echo  FATAL ERROR  %IMPORT_CMD_TXT% does not exist
+    echo  The package is not complete
+    pause
+    exit /B 3
+)
+
+rem  ===== Execution ========================================================
+
+%KMS_COPY_EXE% . %DST% %IMPORT_CMD_TXT%
+if ERRORLEVEL 1 (
+    echo  ERROR  %KMS_COPY_EXE% . %DST% %IMPORT_CMD_TXT%  reported an error - %ERRORLEVEL%
+    echo  The package is not complete
+    pause
+    exit /B 4
+)
+
+rem  ===== End ==============================================================
+
+echo  OK

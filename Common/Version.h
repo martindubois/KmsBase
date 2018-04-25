6b8fafd0e69cd422680c050c9d6ffb6454ad9779
Martin Dubois <mdubois@kms-quebec.com>
2018-04-25 12:48:17 -0400

    2.4.19

--- Common/Version.h
+++ Common/Version.h
@@ -10,11 +10,11 @@
 
 #define  VERSION_MAJOR          (2)
 #define  VERSION_MINOR          (4)
-#define  VERSION_BUILD          (18)
+#define  VERSION_BUILD          (19)
 #define  VERSION_COMPATIBILITY  (0)
 
-#define  VERSION_RC    2,4,18,0
-#define	 VERSION_STR   "2.4.18.0"
-#define	 VERSION_STR0  "2.4.18.0\0"
+#define  VERSION_RC    2,4,19,0
+#define	 VERSION_STR   "2.4.19.0"
+#define	 VERSION_STR0  "2.4.19.0\0"
 
 #define  VERSION_TYPE  "KI"

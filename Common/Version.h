476cfd1634e5df75867fa0190f38ba7bb3d96405
Martin Dubois <mdubois@kms-quebec.com>
2019-05-07 11:34:58 -0400

    3.0.13_Windows

--- Common/Version.h
+++ Common/Version.h
@@ -10,11 +10,11 @@
 
 #define  VERSION_MAJOR          ( 3)
 #define  VERSION_MINOR          ( 0)
-#define  VERSION_BUILD          (12)
+#define  VERSION_BUILD          (13)
 #define  VERSION_COMPATIBILITY  ( 0)
 
-#define  VERSION_RC    3,0,12,0
-#define	 VERSION_STR   "3.0.12.0"
-#define	 VERSION_STR0  "3.0.12.0\0"
+#define  VERSION_RC    3,0,13,0
+#define	 VERSION_STR   "3.0.13.0"
+#define	 VERSION_STR0  "3.0.13.0\0"
 
-#define  VERSION_TYPE  "KI"
+#define  VERSION_TYPE  ""

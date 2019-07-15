eb513f4af9e7697fefc3418f34cff21bd0d1ca77
Martin Dubois <mdubois@kms-quebec.com>
2019-07-15 09:04:46 -0400

    KMS_TOOL_BANNER working on Linux

--- Includes/KmsTool.h
+++ Includes/KmsTool.h
@@ -1,15 +1,17 @@
 
-// Product / Produit  KmsBase
+// Product  KmsBase
 
 /// \author  KMS - Martin Dubois, ing.
 /// \file    Includes/KmsTool.h
+
+// CODE REVIEW  2019-07-15  KMS - Martin Dubois, ing.
 
 #pragma once
 
 // Macros
 /////////////////////////////////////////////////////////////////////////////
 
-#ifdef _DEBUG
+#if defined( _DEBUG ) || defined( NDEBUG )
 
     #define KMS_TOOL_BANNER(P,C,V,T)       \
         printf( P " - " C "\n" );          \
@@ -23,4 +25,4 @@
         printf( "Version " V " " T " (Debug)\n" ); \
         printf( "Compiled at " __TIME__ " on " __DATE__ "\n" );
 
-#endif // _DEBUG
+#endif

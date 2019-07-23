ee1ee48cf735572ec29cdabfc2deeba4494671e3
Martin Dubois <mdubois@kms-quebec.com>
2019-07-23 14:24:05 -0400

    Exception - Used defined code and review

--- KmsLib/Exception.cpp
+++ KmsLib/Exception.cpp
@@ -3,6 +3,10 @@
 // Product  KmsBase
 // File     KmsLib/Exception.cpp
 
+// CODE REVIEW    2019-07-23  KMS - Martin Dubois, ing.
+
+// TEST COVERAGE  2019-07-23  KMS - Martin Dubois, ing.
+
 // Includes
 /////////////////////////////////////////////////////////////////////////////
 
@@ -10,6 +14,9 @@
 
 // ===== C ==================================================================
 #include <assert.h>
+
+// ===== C++ ================================================================
+#include <map>
 
 #ifdef _KMS_WINDOWS_
     // ===== Windows ========================================================
@@ -19,95 +26,100 @@
 // ===== Includes ===========================================================
 #include <KmsLib/Exception.h>
 
-// Constantes
+// Constants
 /////////////////////////////////////////////////////////////////////////////
 
 static const char * CODE_NAMES[ KmsLib::Exception::CODE_QTY ] =
 {
-	// ----- 2.0 -------------------------------------------------------------
-	"CODE_IO_ERROR"				,
-	"CODE_IOCTL_ERROR"			,
-	"CODE_NO_SUCH_DEVICE"		,
-	"CODE_NOT_ENOUGH_MEMORY"	,
-	"CODE_PRIVATE"				,
-	"CODE_REGISTRY_ERROR"		,
-	"CODE_SETUP_API_ERROR"		,
-	"CODE_UNKNOWN"				,
-	"CODE_USER_ERROR"			,
-	
-	// ----- 2.1 -------------------------------------------------------------
-	"CODE_AUTHENTIFICATION_ERROR"	,
-	"CODE_COMMUNICATION_ERROR"		,
-	"CODE_GUI_ERROR"				,
-	"CODE_HARDWARE_ERROR"			,
-	"CODE_INSTALLATION_ERROR"		,
-	"CODE_INVALID_ARGUMENT"			,
-	"CODE_INVALID_BUFFER_ADDRESS"	,
-	"CODE_INVALID_BUFFER_SIZE"		,
-	"CODE_INVALID_DATA"				,
-	"CODE_INVALID_DATE"				,
-	"CODE_INVALID_HANDLE"			,
-	"CODE_INVALID_TIME"				,
-	"CODE_NETWORK_ERROR"			,
-	"CODE_NOT_FOUND"				,
-	"CODE_OVERFLOW"					,
-	"CODE_PERMISSION_DENIED"		,
-	"CODE_PROTOCOL_ERROR"			,
-	"CODE_STATE_ERROR"				,
-	"CODE_SYSTEM_ERROR"				,
-	"CODE_THREAD_ERROR"				,
-	"CODE_TIMEOUT"					,
-	"CODE_UNDERFLOW"				,
+    "CODE_IO_ERROR"         , // 0
+    "CODE_IOCTL_ERROR"      ,
+    "CODE_NO_SUCH_DEVICE"   ,
+    "CODE_NOT_ENOUGH_MEMORY",
+    "CODE_PRIVATE"          ,
+    "CODE_REGISTRY_ERROR"   , // 5
+    "CODE_SETUP_API_ERROR"  ,
+    "CODE_UNKNOWN"          ,
+    "CODE_USER_ERROR"       ,
 
-	// ----- 2.2 ----------------------------------------------------
-	"CODE_SERVICE_MANAGER_ERROR"	,
-	"CODE_SYSTEM_LOG_ERROR"			,
+    "CODE_AUTHENTIFICATION_ERROR",
+    "CODE_COMMUNICATION_ERROR"   , // 10
+    "CODE_GUI_ERROR"             ,
+    "CODE_HARDWARE_ERROR"        ,
+    "CODE_INSTALLATION_ERROR"    ,
+    "CODE_INVALID_ARGUMENT"      ,
+    "CODE_INVALID_BUFFER_ADDRESS", // 15
+    "CODE_INVALID_BUFFER_SIZE"   ,
+    "CODE_INVALID_DATA"          ,
+    "CODE_INVALID_DATE"          ,
+    "CODE_INVALID_HANDLE"        ,
+    "CODE_INVALID_TIME"          , // 20
+    "CODE_NETWORK_ERROR"         ,
+    "CODE_NOT_FOUND"             ,
+    "CODE_OVERFLOW"              ,
+    "CODE_PERMISSION_DENIED"     ,
+    "CODE_PROTOCOL_ERROR"        , // 25
+    "CODE_STATE_ERROR"           ,
+    "CODE_SYSTEM_ERROR"          ,
+    "CODE_THREAD_ERROR"          ,
+    "CODE_TIMEOUT"               ,
+    "CODE_UNDERFLOW"             , // 30
 
-	// ----- 2.3 ----------------------------------------------------
-	"CODE_ACCESS_VIOLATION"			,
-	"CODE_INTEGER_DIVIDE_BY_ZERO"	,
-	"CODE_INVALID_COMMAND_LINE"		,
-	"CODE_OPEN_CL_ERROR"			,
-	"CODE_STRUCTURED_EXCEPTION"		,
+    "CODE_SERVICE_MANAGER_ERROR",
+    "CODE_SYSTEM_LOG_ERROR"     ,
 
-    // ----- 2.6 ----------------------------------------------------
-    "CODE_CLOSE_HANDLE_ERROR"       ,
-    "CODE_BUFFER_TOO_SMALL"         ,
-    "CODE_COM_PORT_ERROR"           ,
-    "CODE_COPY_FILE_ERROR"          ,
-    "CODE_CREATE_FILE_ERROR"        ,
-    "CODE_DELETE_FILE_ERROR"        ,
-    "CODE_DENIED"                   ,
-    "CODE_DIRECTORY_ERROR"          ,
-    "CODE_ERROR"                    ,
-    "CODE_FILE_OPEN_ERROR"          ,
-    "CODE_FILE_READ_ERROR"          ,
-    "CODE_FILE_WRITE_ERROR"         ,
-    "CODE_INVALID_ADDRESS"          ,
-    "CODE_INVALID_PARAMETER"        ,
-    "CODE_INVALID_PORT_NUMBER"      ,
-    "CODE_IO_CANCEL_ERROR"          ,
-    "CODE_MOVE_FILE_ERROR"          ,
-    "CODE_NO_COM_PORT"              ,
-    "CODE_NOT_IMPLEMENTED"          ,
-    "CODE_READ_FILE_ERROR"          ,
-    "CODE_REG_CLOSE_ERROR"          ,
-    "CODE_REG_CREATE_ERROR"         ,
-    "CODE_REG_DELETE_ERROR"         ,
-    "CODE_REG_OPEN_ERROR"           ,
-    "CODE_REG_QUERY_ERROR"          ,
-    "CODE_REG_SET_ERROR"            ,
-    "CODE_SOCKET_ERROR"             ,
-    "CODE_TO_DEFINE"                ,
-    "CODE_UNSUCCESSFUL"             ,
-    "CODE_WINSOCK_ERROR"            ,
-    "CODE_WRITE_FILE_ERROR"         ,
+    "CODE_ACCESS_VIOLATION"      ,
+    "CODE_INTEGER_DIVIDE_BY_ZERO",
+    "CODE_INVALID_COMMAND_LINE"  , // 35
+    "CODE_OPEN_CL_ERROR"         ,
+    "CODE_STRUCTURED_EXCEPTION"  ,
+
+    "CODE_CLOSE_HANDLE_ERROR" ,
+    "CODE_BUFFER_TOO_SMALL"   ,
+    "CODE_COM_PORT_ERROR"     , // 40
+    "CODE_COPY_FILE_ERROR"    ,
+    "CODE_CREATE_FILE_ERROR"  ,
+    "CODE_DELETE_FILE_ERROR"  ,
+    "CODE_DENIED"             ,
+    "CODE_DIRECTORY_ERROR"    , // 45
+    "CODE_ERROR"              ,
+    "CODE_FILE_OPEN_ERROR"    ,
+    "CODE_FILE_READ_ERROR"    ,
+    "CODE_FILE_WRITE_ERROR"   ,
+    "CODE_INVALID_ADDRESS"    , // 50
+    "CODE_INVALID_PARAMETER"  ,
+    "CODE_INVALID_PORT_NUMBER",
+    "CODE_IO_CANCEL_ERROR"    ,
+    "CODE_MOVE_FILE_ERROR"    ,
+    "CODE_NO_COM_PORT"        , // 55
+    "CODE_NOT_IMPLEMENTED"    ,
+    "CODE_READ_FILE_ERROR"    ,
+    "CODE_REG_CLOSE_ERROR"    ,
+    "CODE_REG_CREATE_ERROR"   ,
+    "CODE_REG_DELETE_ERROR"   , // 60
+    "CODE_REG_OPEN_ERROR"     ,
+    "CODE_REG_QUERY_ERROR"    ,
+    "CODE_REG_SET_ERROR"      ,
+    "CODE_SOCKET_ERROR"       ,
+    "CODE_TO_DEFINE"          , // 65
+    "CODE_UNSUCCESSFUL"       ,
+    "CODE_WINSOCK_ERROR"      ,
+    "CODE_WRITE_FILE_ERROR"   , // 58
 };
 
-// Static method declarations / Declaration des fonctions statique
+// Data type
 /////////////////////////////////////////////////////////////////////////////
 
-// ===== Entry points / Points d'entres =====================================
+typedef std::map< KmsLib::Exception::Code, std::string > NameMap;
+
+// Static variables
+/////////////////////////////////////////////////////////////////////////////
+
+static NameMap sNames;
+
+// Static method declarations
+/////////////////////////////////////////////////////////////////////////////
+
+// ===== Entry points =======================================================
 
 #ifdef _KMS_WINDOWS_
     static void TranslateException(unsigned int aCode, struct _EXCEPTION_POINTERS * aStruct );
@@ -122,30 +134,65 @@ namespace KmsLib
 	void * Exception::RegisterTranslator()
 	{
 		#if defined( _KMS_LINUX_ ) || defined( _KMS_OS_X_ )
-		return NULL;
-		#endif // _KMS_LINUX_ || _KMS_OS_X_
+		    return NULL;
+		#endif
 
 		#ifdef _KMS_WINDOWS_
-		return _set_se_translator(TranslateException);
-		#endif // _KMS_WINDOWS_
+		    return _set_se_translator(TranslateException);
+		#endif
 	}
 
 	void Exception::RestoreTranslator(void * aTranslator)
 	{
 		#ifdef _KMS_WINDOWS_
-		void * lTranslator = _set_se_translator(reinterpret_cast<_se_translator_function>(aTranslator));
-		assert(TranslateException == lTranslator);
-		#endif // _KMS_WINDOWS_
+		    void * lTranslator = _set_se_translator(reinterpret_cast<_se_translator_function>(aTranslator));
+		    assert(TranslateException == lTranslator);
+		#endif
 	}
+
+    Exception::Code Exception::DefineCode(unsigned int aIndex, const char * aName)
+    {
+        Code lResult = static_cast<Code>(CODE_BASE + aIndex);
+
+        SetCodeName(lResult, aName);
+
+        return lResult;
+    }
 
     const  char  * Exception::GetCodeName(Code  aCode)
     {
+        NameMap::iterator lIt = sNames.find(aCode);
+        if (sNames.end() != lIt)
+        {
+            return lIt->second.c_str();
+        }
+
         if (CODE_QTY <= aCode)
         {
             return  NULL;
         }
 
         return  CODE_NAMES[aCode];
+    }
+
+    void Exception::SetCodeName(Code aCode, const char * aName)
+    {
+        if (NULL == aName)
+        {
+            sNames.erase(aCode);
+        }
+        else
+        {
+            NameMap::iterator lIt = sNames.find(aCode);
+            if (sNames.end() == lIt)
+            {
+                sNames.insert(NameMap::value_type(aCode, aName));
+            }
+            else
+            {
+                lIt->second = aName;
+            }
+        }
     }
 
 	Exception::Exception(Code aCode, const char * aWhat, const char * aMessage, const char * aFile, const char * aFunction, unsigned int aLine, unsigned int aInfoA) :
@@ -194,27 +241,13 @@ namespace KmsLib
 		assert(NULL != aFile);
 
 		fprintf(aFile,
-			"    Exception - ");
-
-		if (CODE_QTY > mCode)
-		{
-			fprintf(aFile, "%s", CODE_NAMES[mCode]);
-		}
-		else
-		{
-			// NOT TESTED  KmsLib.Exception
-            //             Invalid code / Code invalide<br>
-            //             Not easy to test / Pas facile a tester
-			fprintf(aFile, "%u (0x%08x)", mCode, mCode);
-		}
-
-		fprintf(aFile,
-			"\n"
+            "    Exception - %s (%u, 0x%08x)\n"
 			"        File       : %s (line %u)\n"
 			"        Function   : %s\n"
 			"        Info A     : %u (0x%08x)\n"
 			"        Last error : %u (0x%08x)\n",
-			mFile, mLine,
+            GetCodeName(mCode), mCode, mCode,
+            mFile, mLine,
 			mFunction,
 			mInfoA, mInfoA,
 			mLastError, mLastError );
@@ -247,10 +280,10 @@ namespace KmsLib
 	
 }
 
-// Static functions / Fonction statiques
+// Static functions
 /////////////////////////////////////////////////////////////////////////////
 
-// ===== Entry points / Points d'entres =====================================
+// ===== Entry points =======================================================
 
 #ifdef _KMS_WINDOWS_
 
@@ -263,18 +296,16 @@ namespace KmsLib
 
         switch (aCode)
         {
-            case 0xc0000005: lCode = KmsLib::Exception::CODE_ACCESS_VIOLATION	   ; lMsg = "Access violation"   ; break;
-            case 0xc0000094: lCode = KmsLib::Exception::CODE_INTEGER_DIVIDE_BY_ZERO; lMsg = "Integet divide by 0"; break;
+        case 0xc0000005: lCode = KmsLib::Exception::CODE_ACCESS_VIOLATION	   ; lMsg = "Access violation"   ; break;
+        case 0xc0000094: lCode = KmsLib::Exception::CODE_INTEGER_DIVIDE_BY_ZERO; lMsg = "Integet divide by 0"; break;
 
-            default:
-                // NOT TESTED  KmsLib.Exception
-                //             Unexpected structured exception / Exception
-                //             structure non attendu<br>
-                //             Not easy to test / Pas facile a tester
-                lCode = KmsLib::Exception::CODE_STRUCTURED_EXCEPTION; lMsg = "Structured exception";
+        default:
+            // NOT TESTED  KmsLib.Exception
+            //             Unexpected structured exception
+            lCode = KmsLib::Exception::CODE_STRUCTURED_EXCEPTION; lMsg = "Structured exception";
         }
 
         throw new KmsLib::Exception(lCode, lMsg, NULL, __FILE__, __FUNCTION__, __LINE__, aCode);
     }
 
-#endif // _KMS_WINDOWS_
+#endif
--- KmsLib_Test/Exception.cpp
+++ KmsLib_Test/Exception.cpp
@@ -22,57 +22,95 @@
 
 KMS_TEST_BEGIN(Exception_Base)
 {
+    // ===== RegisterTranslator =============================================
+
+    void * lPrevTranslator = KmsLib::Exception::RegisterTranslator();
+
+    #ifdef _KMS_WINDOWS_
+
+        try
+        {
+            *reinterpret_cast<unsigned int *>(NULL) = 0;
+
+            KMS_TEST_ERROR();
+        }
+        catch (KmsLib::Exception * eE)
+        {
+            KMS_TEST_ERROR_INFO;
+            eE->Write(stdout);
+            KMS_TEST_COMPARE(KmsLib::Exception::CODE_ACCESS_VIOLATION, eE->GetCode());
+        }
+
+        try
+        {
+            volatile unsigned int lA = 1;
+            volatile unsigned int lB = 0;
+
+            lA /= lB;
+
+            KMS_TEST_ERROR();
+        }
+        catch (KmsLib::Exception * eE)
+        {
+            KMS_TEST_ERROR_INFO;
+            eE->Write(stdout);
+            KMS_TEST_COMPARE(KmsLib::Exception::CODE_INTEGER_DIVIDE_BY_ZERO, eE->GetCode());
+        }
+
+    #endif // _KMS_WINDOWS_
+
+    // ===== RestoreTranslator ==============================================
+    KmsLib::Exception::RestoreTranslator(lPrevTranslator);
+
+    // ===== DefineCode =====================================================
+    KmsLib::Exception::Code lCodeZero = KmsLib::Exception::DefineCode(0, "Zero");
+
+    // ===== GetCodeName ====================================================
+
+    KMS_TEST_COMPARE(0, strcmp("CODE_IO_ERROR", KmsLib::Exception::GetCodeName(KmsLib::Exception::CODE_IO_ERROR)));
+    KMS_TEST_COMPARE(0, strcmp("Zero"         , KmsLib::Exception::GetCodeName(lCodeZero)));
+
+    KMS_TEST_ASSERT(NULL == KmsLib::Exception::GetCodeName(KmsLib::Exception::CODE_QTY));
+
+    // ===== SetCodeName ====================================================
+    KmsLib::Exception::SetCodeName(lCodeZero, "0" );
+    KmsLib::Exception::SetCodeName(lCodeZero, NULL);
+
+    // ===== Exception ======================================================
     KmsLib::Exception * lE = new KmsLib::Exception(KmsLib::Exception::CODE_UNKNOWN, "Unknown", NULL, __FILE__, __FUNCTION__, __LINE__, 0);
+
+    // ===== GetCode ========================================================
     KMS_TEST_COMPARE(KmsLib::Exception::CODE_UNKNOWN, lE->GetCode());
-    KMS_TEST_COMPARE(0, lE->GetInfoA    ());
-    KMS_TEST_COMPARE(__LINE__ - 3, lE->GetLine());
-    KMS_TEST_COMPARE(0, strcmp(__FILE__    , lE->GetFile    ()));
+
+    // ===== GetFile ========================================================
+    KMS_TEST_COMPARE(0, strcmp(__FILE__, lE->GetFile()));
+
+    // ===== GetFunction ====================================================
     KMS_TEST_COMPARE(0, strcmp(__FUNCTION__, lE->GetFunction()));
-    KMS_TEST_COMPARE(0, strcmp("Unknown"   , lE->GetMessage ()));
+
+    // ===== GetInfoA =======================================================
+    KMS_TEST_COMPARE(0, lE->GetInfoA    ());
+
+    // ===== GetLastError ===================================================
 
     #ifdef _KMS_LINUX_
-        KMS_TEST_COMPARE( 25, lE->GetLastError());
+        KMS_TEST_COMPARE(25, lE->GetLastError());
     #endif
 
     #ifdef _KMS_WINDOWS_
         KMS_TEST_COMPARE(0, lE->GetLastError());
     #endif
 
-    void * lPrevTranslator = KmsLib::Exception::RegisterTranslator();
+    // ===== GetLine ========================================================
+    KMS_TEST_COMPARE(__LINE__ - 25, lE->GetLine());
 
-#ifdef _KMS_WINDOWS_
+    // ===== GetMessage =====================================================
+    KMS_TEST_COMPARE(0, strcmp("Unknown"   , lE->GetMessage ()));
 
-    try
-    {
-        *reinterpret_cast<unsigned int *>(NULL) = 0;
+    // ===== Write ==========================================================
+    lE->Write(stdout);
 
-        KMS_TEST_ERROR();
-    }
-    catch (KmsLib::Exception * eE)
-    {
-        KMS_TEST_ERROR_INFO;
-        eE->Write(stdout);
-        KMS_TEST_COMPARE(KmsLib::Exception::CODE_ACCESS_VIOLATION, eE->GetCode());
-    }
-
-    try
-    {
-        volatile unsigned int lA = 1;
-        volatile unsigned int lB = 0;
-
-        lA /= lB;
-
-        KMS_TEST_ERROR();
-    }
-    catch (KmsLib::Exception * eE)
-    {
-        KMS_TEST_ERROR_INFO;
-        eE->Write(stdout);
-        KMS_TEST_COMPARE(KmsLib::Exception::CODE_INTEGER_DIVIDE_BY_ZERO, eE->GetCode());
-    }
-
-#endif // _KMS_WINDOWS_
-
-    KmsLib::Exception::RestoreTranslator(lPrevTranslator);
+    // ===== what ===========================================================
+    KMS_TEST_COMPARE(0, strcmp("Unknown", lE->what()));
 }
 KMS_TEST_END_2

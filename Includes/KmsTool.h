5377e3d2bbc4502a5620b6e5a2993c18e705f773
Martin Dubois <mdubois@kms-quebec.com>
2020-08-17 09:13:38 -0400

    KMS_TOOL_BANNER includes KMS web site URL

--- Includes/KmsTool.h
+++ Includes/KmsTool.h
@@ -1,10 +1,11 @@
 
-// Product  KmsBase
+// License http://www.apache.org/licenses/LICENSE-2.0
+// Product KmsBase
 
-/// \author  KMS - Martin Dubois, ing.
-/// \file    Includes/KmsTool.h
-
-// CODE REVIEW  2019-07-15  KMS - Martin Dubois, ing.
+/// \author    KMS - Martin Dubois, P.Eng.
+/// \copyright Copyright &copy; 2020 KMS
+/// \file      Includes/KmsTool.h
+/// \brief     KMS_TOOL_BANNER
 
 #pragma once
 
@@ -13,16 +14,18 @@
 
 #if ( ! defined( _DEBUG ) ) || defined( NDEBUG )
 
-    #define KMS_TOOL_BANNER(P,C,V,T)       \
-        printf( P " - " C "\n" );          \
-        printf( "Version " V " " T "\n" ); \
-        printf( "Compiled at " __TIME__ " on " __DATE__ "\n" )
+    #define KMS_TOOL_BANNER(P,C,V,T)                            \
+        printf( P " - " C "\n" );                               \
+        printf( "Version " V " " T "\n" );                      \
+        printf( "Compiled at " __TIME__ " on " __DATE__ "\n" ); \
+        printf( "http://www.kms-quebec.com\n")
 
 #else
 
-    #define KMS_TOOL_BANNER(P,C,V,T)               \
-        printf( P " - " C "\n" );                  \
-        printf( "Version " V " " T " (Debug)\n" ); \
-        printf( "Compiled at " __TIME__ " on " __DATE__ "\n" );
+    #define KMS_TOOL_BANNER(P,C,V,T)                            \
+        printf( P " - " C "\n" );                               \
+        printf( "Version " V " " T " (Debug)\n" );              \
+        printf( "Compiled at " __TIME__ " on " __DATE__ "\n" ); \
+        printf( "http://www.kms-quebec.com\n")
 
 #endif

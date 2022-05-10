32ddc3dd0cbf098a4ae84de4f658363367393bc4
Martin Dubois <mdubois@kms-quebec.com>
2022-05-10 11:11:17 -0400

    2.0.9

--- Common/Version.h
+++ Common/Version.h
@@ -1,11 +1,13 @@
 
-// Auteur	:	KMS -	Martin Dubois, ing.
-// Projet	:	KmsBase
-// Fichier	:	Common/Version.h
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KmsBase
+// File      Common/Version.h
 
 #pragma once
 
 // Constantes
 /////////////////////////////////////////////////////////////////////////////
 
-#define	VERSION_STR	"2.0.8.0"
+#define	VERSION_STR	"2.0.9.0"
--- Export.cmd
+++ Export.cmd
@@ -1,8 +1,10 @@
 @echo off
 
-rem  Auteur		:	KMS -	Martin Dubois, ing.
-rem  Projet		:	KmsBase
-rem  Fichier	:	Export.cmd
+rem Author    KMS - Martin Dubois, P. Eng.
+rem Copyright (C) 2022 KMS
+rem License   http://www.apache.org/licenses/LICENSE-2.0
+rem Product   KmsBase
+rem File	  Export.cmd
 
 echo  Execution de Export.cmd %1 ...
 
@@ -37,19 +39,6 @@ mkdir "%DST%\Libraries\Release_32"
 mkdir "%DST%\Libraries\Release_32_DLL"
 mkdir "%DST%\Libraries\Release_64"
 mkdir "%DST%\Libraries\Release_64_DLL"
-mkdir "%DST%\Templates"
-mkdir "%DST%\Templates\DLL"
-mkdir "%DST%\Templates\DLL\_DocUser"
-mkdir "%DST%\Templates\Driver_KMDF"
-mkdir "%DST%\Templates\Driver_KMDF\_DocUser"
-mkdir "%DST%\Templates\Solution"
-mkdir "%DST%\Templates\Solution\_DocUser"
-mkdir "%DST%\Templates\Test"
-mkdir "%DST%\Templates\Test\_DocUser"
-mkdir "%DST%\Templates\Tool"
-mkdir "%DST%\Templates\Tool\_DocUser"
-mkdir "%DST%\Templates\Solution"
-mkdir "%DST%\Templates\Solution\_DocUser"
 
 xcopy /IKQS Debug\KmsLib.lib						"%DST%\Libraries\Debug_32"
 xcopy /IKQS Debug_DLL\KmsLib.lib					"%DST%\Libraries\Debug_32_DLL"
@@ -57,21 +46,6 @@ xcopy /IKQS Includes\*.h							"%DST%\Includes"
 xcopy /IKQS Release\KmsLib.lib						"%DST%\Libraries\Release_32"
 xcopy /IKQS Release_DLL\KmsLib.lib					"%DST%\Libraries\Release_32_DLL"
 xcopy /IKQS Scripts\*.cmd							"%DST%"
-xcopy /IKQS Templates\DLL\*.cpp						"%DST%\Templates\DLL"
-xcopy /IKQS Templates\DLL\*.h						"%DST%\Templates\DLL"
-xcopy /IKQS Templates\DLL\_DocUser\*.txt			"%DST%\Templates\DLL\_DocUser"
-xcopy /IKQS Templates\Driver_KMDF\*.cpp				"%DST%\Templates\Driver_KMDF"
-xcopy /IKQS Templates\Driver_KMDF\*.h				"%DST%\Templates\Driver_KMDF"
-xcopy /IKQS Templates\Driver_KMDF\*.inf				"%DST%\Templates\Driver_KMDF"
-xcopy /IKQS Templates\Driver_KMDF\_DocUser\*.txt	"%DST%\Templates\Driver_KMDF\_DocUser"
-xcopy /IKQS Templates\Solutions\.gitignore			"%DST%\Templates\Solution"
-xcopy /IKQS Templates\Solutions\*.cmd				"%DST%\Templates\Solution"
-xcopy /IKQS Templates\Solutions\_DocUser\*.txt		"%DST%\Templates\Solution\_DocUser"
-rem  TODO : xcopy /IKQS Templates\Test\*.cpp			"%DST%\Templates\Test"
-rem  TODO : xcopy /IKQS Templates\Test\_DocUser\*.txt	"%DST%\Templates\Test\_DocUser"
-xcopy /IKQS Templates\Tool\*.cpp					"%DST%\Templates\Tool"
-xcopy /IKQS Templates\Tool\*.h						"%DST%\Templates\Tool"
-xcopy /IKQS Templates\Tool\_DocUser\*.txt			"%DST%\Templates\Tool\_DocUser"
 xcopy /IKQS x64\Debug\KmsLib.lib					"%DST%\Libraries\Debug_64"
 xcopy /IKQS x64\Debug_DLL\KmsLib.lib				"%DST%\Libraries\Debug_64_DLL"
 xcopy /IKQS x64\Release\KmsLib.lib					"%DST%\Libraries\Release_64"
